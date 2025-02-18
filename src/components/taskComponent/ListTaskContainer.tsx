0c120927ec9f2381d4f8ccbc17cf89df8e2f0cbe
Ar-Akhil <ar.akhil1511@gmail.com>
2025-02-18 22:04:18 +0530

    Feat: Added getTask functionality

--- src/components/taskComponent/ListTaskContainer.tsx
+++ src/components/taskComponent/ListTaskContainer.tsx
@@ -1,14 +1,44 @@
 import Task from "./ListTasks";
+import { RootState } from "../../redux/store";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch } from "../../redux/store";
+import { fetchTasks } from "../../redux/dataSlice";
+import { useEffect } from "react";
+
 const TaskContainer: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const tasks = useSelector((state: RootState) => state.tasks.tasks);
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+
+  const categorizedTasks = {
+    "To-Do": tasks.filter((task) => task.status === "To-Do"),
+    "In-Progress": tasks.filter((task) => task.status === "In-Progress"),
+    Completed: tasks.filter((task) => task.status === "Completed"),
+  };
+
   return (
     <>
-      <Task title={"Todo"} bgColor={"hsl(320, 100%, 90%)"} taskCount={0} />
+      <Task
+        title={"Todo"}
+        bgColor={"hsl(320, 100%, 90%)"}
+        taskCount={categorizedTasks["To-Do"].length}
+        tasks={categorizedTasks["To-Do"]}
+      />
       <Task
         title={"In-Progress"}
         bgColor={"hsl(187, 64%, 88%)"}
-        taskCount={0}
+        taskCount={categorizedTasks["In-Progress"].length}
+        tasks={categorizedTasks["In-Progress"]}
       />
-      <Task title={"Completed"} bgColor={"hsl(78, 53%, 90%)"} taskCount={0} />
+      <Task
+        title={"Completed"}
+        bgColor={"hsl(78, 53%, 90%)"}
+        taskCount={categorizedTasks["Completed"].length}
+        tasks={categorizedTasks["Completed"]}
+      />
     </>
   );
 };
--- src/components/taskComponent/ListTasks.tsx
+++ src/components/taskComponent/ListTasks.tsx
@@ -1,26 +1,29 @@
 import { ChevronUpIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
+import { CheckCircleIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
+import { TaskData } from "../AddTask";
 
 interface TaskProps {
+  tasks: TaskData[];
   title: string;
   bgColor: string;
   taskCount: number;
 }
 
-const Task: React.FC<TaskProps> = ({ title, bgColor, taskCount }) => {
+const Task: React.FC<TaskProps> = ({ title, bgColor, taskCount, tasks }) => {
   const [toggle, setToggle] = useState<boolean>(true);
 
   return (
     <div className="mb-8">
       <div
-        className={`flex justify-between items-center font-bold py-2 px-3  ${
+        className={`flex justify-between items-center font-bold py-2 px-3 ${
           !toggle ? "rounded-lg" : "rounded-tl-lg rounded-tr-lg"
         } cursor-pointer`}
         style={{ backgroundColor: bgColor }}
         onClick={() => setToggle(!toggle)}
       >
         <p className="select-none">
-          {title}({taskCount})
+          {title} ({taskCount})
         </p>
         {toggle ? (
           <ChevronDownIcon className="size-4 stroke-3" />
@@ -30,9 +33,29 @@ const Task: React.FC<TaskProps> = ({ title, bgColor, taskCount }) => {
       </div>
       {toggle && (
         <div className="bg-gray-100 rounded-b-lg">
-          <div className="flex justify-center items-center h-40">
-            <p className="text-sm select-none">No Task in {title}</p>
-          </div>
+          {tasks.length > 0 ? (
+            <ul>
+              {tasks.map((task) => (
+                <li key={task.id} className="py-1">
+                  <div className="grid grid-cols-10 grid-rows-1 border-b-2 border-black/5 p-1 font-semibold items-center">
+                    <div className="col-span-3 flex items-center gap-1.5">
+                      <CheckCircleIcon className="size-5 text-gray-400" />
+                      <p>{task.title}</p>
+                    </div>
+                    <p className="col-span-2">{task.dueDate}</p>
+                    <p className="col-span-2 bg-gray-300 w-fit px-2 py-1 rounded">
+                      {task.status}
+                    </p>
+                    <p className="col-span-3">{task.category}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div className="flex justify-center items-center h-40">
+              <p className="text-sm select-none">No Task in {title}</p>
+            </div>
+          )}
         </div>
       )}
     </div>
